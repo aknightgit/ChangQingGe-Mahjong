f52e141600534254b0311f24ee389664daad7d4b
ChangQingGe <changqingge@mahjong.dev>
2026-04-04 01:40:40 +0800

    ui: 左右弃牌区位置微调 - 下移10%, 左家左移5%, 右家右移5%

--- app/pages/gameroom/[roomId].vue
+++ app/pages/gameroom/[roomId].vue
@@ -2426,13 +2426,13 @@ const forceDiscard = async (p: Player) => {
   transform: translateX(-50%);
 }
 :deep(.discard-zone--left) {
-  top: 40%;
-  left: calc(21.6% + 36px);
+  top: 50%;
+  left: calc(16.6% + 36px);
   transform: translateY(-50%) rotate(90deg);
 }
 :deep(.discard-zone--right) {
-  top: 40%;
-  right: calc(21.6% + 36px);
+  top: 50%;
+  right: calc(26.6% + 36px);
   transform: translateY(-50%) rotate(-90deg);
 }
 
